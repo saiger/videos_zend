7fb604df2975c2121d22a9871fa0983b23b9b8ee
Sergey Kozirev <sergey@test.com>
2014-02-17 11:33:12 +0400

    Add new Add, Delete and Edit pages for Videos site

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,13 +6,35 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+
     }
 
     public function indexAction()
     {
-        echo "I am in IndexController,  I am in indexAction!!!";
+        echo "ß â IndexController,  I am in indexAction!!!";
+    }
+
+    public function addAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
     }
 
 
 }
+
+
+
+
+
+
 
