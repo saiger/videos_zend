dd11482b8ddb130e36ff3fdbb5ec4cebe28e02e9
Sergey Kozirev <sergey@test.com>
2014-02-24 10:45:27 +0400

    Adding new entries

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -9,8 +9,7 @@ class IndexController extends Zend_Controller_Action
 
     }
 
-    public function indexAction()
-    {
+    public function indexAction(){
         // Создаём объект нашей модели
         $movies = new Application_Model_DbTable_Movies();
 
@@ -20,9 +19,46 @@ class IndexController extends Zend_Controller_Action
 
     }
 
-    public function addAction()
-    {
-        // action body
+    public function addAction(){
+        // Создаём форму
+        $form = new Application_Form_Movie();
+
+        // Указываем текст для submit
+        $form->submit->setLabel('Add');
+
+        // Передаём форму в view
+        $this->view->form = $form;
+
+        // Если к нам идёт Post запрос
+        if ($this->getRequest()->isPost()) {
+
+            // Принимаем его
+            $formData = $this->getRequest()->getPost();
+
+            // Если форма заполнена верно
+            if ($form->isValid($formData)) {
+
+                // Извлекаем режиссёра
+                $director = $form->getValue('director');
+
+                // Извлекаем название фильма
+                $title = $form->getValue('title');
+
+                // Создаём объект модели
+                $movies = new Application_Model_DbTable_Movies();
+
+                // Вызываем метод модели addMovie для вставки новой записи
+                $movies->addMovie($director, $title);
+
+                // Используем библиотечный helper для редиректа на action = index
+                $this->_helper->redirector('index');
+            } else {
+            // Если форма заполнена неверно,
+            // используем метод populate для заполнения всех полей
+            // той информацией, которую ввёл пользователь
+            $form->populate($formData);
+            }
+        }
     }
 
     public function editAction()
